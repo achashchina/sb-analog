f76472b108a3ebc74184e11dd7952e793f265b62
viraresh <viraresh@ukr.net>
2025-02-24 14:59:00 +0200

    feature-portfolio: - added page portfolio - added package-lock.json to .gitignore - add animation to block

--- src/app/pages/portfolio/index.page.ts
+++ src/app/pages/portfolio/index.page.ts
@@ -1,5 +1,15 @@
-import { Component } from '@angular/core';
-import {NgForOf, NgIf} from "@angular/common";
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import {isPlatformBrowser, NgForOf, NgIf} from "@angular/common";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+interface Project {
+  img: string;
+  name: string;
+  description: string;
+  techStack: string[];
+  industry: string;
+}
 
 @Component({
   selector: 'sb-portfolio',
@@ -10,39 +20,54 @@ import {NgForOf, NgIf} from "@angular/common";
     NgIf
   ]
 })
-export default class PortfolioPage {
-  projects = [
-    {
-      title: 'Angular SaaS Healthcare Project',
-      description: 'SaaS Healthcare Project with Angular framework as frontend',
-      techStack: ['Angular', 'HTML5', 'CSS3', 'Web Development'],
-      industry: 'Healthcare',
-      image: '../public/images/portfolio/healthcare-project.png',
-      link: '#'
-    },
-    {
-      title: 'LMS for Math Courses',
-      description: 'This LMS was built with Angular. My key goal was optimization rxjs streams',
-      techStack: ['Angular', 'RxJs', 'Web Application Development', 'Web Development', 'Firebase'],
-      industry: '',
-      image: '../public/images/portfolio/math_courses.png',
-      link: '#'
-    },
-    {
-      title: 'Angular application with oAuth',
-      description: 'Angular application dashboard with oAuth, MongoDB, Express.js',
-      techStack: ['Angular', 'Express', 'Mongo DB', 'Front-End Development', 'Web API'],
-      industry: '',
-      image: '../public/images/portfolio/oAuth-app.png',
-      link: '#'
-    },
-    {
-      title: 'Application for Vispo',
-      description: 'collaboration between investors & founders',
-      techStack: ['MEAN Stack', 'Angular', 'RxJs', 'Tailwind CSS', 'Java Script'],
-      industry: '',
-      image: '../public/images/portfolio/vispo_project.png',
-      link: '#'
+export default class PortfolioPage implements OnInit {
+  projects?: Project[];
+  isBrowser: boolean;
+
+  constructor(private httpClient: HttpClient, @Inject(PLATFORM_ID) private platformId: any) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
+
+  ngOnInit(): void {
+    if (this.isBrowser) {
+      this.fetchProjects().subscribe(({ projects }) => {
+        this.projects = projects;
+        setTimeout(() => this.observeProjects(), 500);
+      });
     }
-  ];
+  }
+
+  fetchProjects(): Observable<{ projects: Project[] }> {
+    return this.httpClient.get<{ projects: Project[] }>('/data/projects.json');
+  }
+
+  private observeProjects() {
+    if (!this.isBrowser || typeof IntersectionObserver === 'undefined') {
+      return;
+    }
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('in-view');
+            entry.target.classList.remove('not-in-view');
+          } else {
+            entry.target.classList.remove('in-view');
+            entry.target.classList.add('not-in-view');
+          }
+        });
+      },
+      {
+        rootMargin: '0px',
+        threshold: 0.2,
+      }
+    );
+
+    setTimeout(() => {
+      const tags = document.querySelectorAll('.to-animate');
+      tags.forEach((tag) => observer.observe(tag));
+    }, 100);
+
+  }
 }
