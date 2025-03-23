5f0fb973de0a9257ca1652dc15acf9401d7fe61d
achashchina <chaschina.a.lv@gmail.com>
2025-03-23 22:00:31 +0200

    added contact form

--- src/app/pages/portfolio/[slug].page.ts
+++ src/app/pages/portfolio/[slug].page.ts
@@ -3,11 +3,12 @@ import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { map, switchMap } from 'rxjs';
 import { PortfolioService } from './portfolio.service';
+import { ContactFormComponent } from '../../components/contact-form/contact-form.component';
 
 @Component({
   selector: 'async-portfolio-details',
   standalone: true,
-  imports: [AsyncPipe, CommonModule],
+  imports: [AsyncPipe, CommonModule, ContactFormComponent],
   templateUrl: 'portfolio-details.component.html',
   styleUrl: 'portfolio.component.scss',
   encapsulation: ViewEncapsulation.None,
